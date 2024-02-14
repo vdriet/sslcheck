41109d3198b39da570f8cba4d3c957ba72bdb188
Peter <Peter@chromebook>
2024-02-14 22:48:06 +0100

    run detached

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd /home/peter/dev/sslcheck
 sudo netstat -tulpn | grep 8082 | awk '{print $7}' | sed 's/\/python//g' | xargs sudo kill -9 | true
 . ./env.sh
-python . >> /home/peter/logs/sslcheck.log
+python . >> /home/peter/logs/sslcheck.log &
