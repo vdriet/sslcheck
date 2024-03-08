82d1bb4682f14d0d303e9da10a91f8868011236d
Peter <peter@kokosnoot>
2024-03-08 13:51:36 +0100

    verbeteringen tbv run in /opt

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-cd /home/peter/dev/sslcheck
 sudo netstat -tulpn | grep 8082 | awk '{print $7}' | sed 's/\/python//g' | xargs sudo kill -9 | true
 . ./env.sh
 python . >> /home/peter/logs/sslcheck.log &
