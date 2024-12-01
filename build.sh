33e1328ed6fe4874393c447a70fae29f31fc219b
Peter <peter@chromebook>
2024-12-01 20:04:50 +0100

    IT-9 voorbereidingen voor testen

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 pip install -r requirements.txt
-pip install setuptools pip --upgrade
 pip list --outdated
 pylint *.py
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sudo netstat -tulpn | grep 8082 | awk '{print $7}' | sed 's/\/python//g' | xargs sudo kill -9 | true
 . ./env.sh
-python . >> /home/peter/logs/sslcheck.log &
+python sslcheck.py >> /home/peter/logs/sslcheck.log &
